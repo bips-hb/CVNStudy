51a84aa2ce2ca687f2bb5c983d1e189857c2ec21
Louis Dijkstra <louisdijkstra@gmail.com>
2023-02-22 14:16:32 +0100

    Added function that determines the best choice of lambda1,lambda2 for whatever you want to optimize (aic, bic, F1)

--- process-results.R
+++ process-results.R
@@ -2,19 +2,29 @@ library(readr)
 library(dplyr)
 
 # Function that take in the raw results stored 
-r <- readr::read_rds("results/raw-results.rds")
+data <- readr::read_rds("results/raw-results.rds")
 
 #' There are different 
 #'  * parameter settings for the simulation
 #'  * different versions of the weight matrix
-#'  * 9 graphs 
 #'  * different lambda values
 
-
-colnames(r)
-
-k <- r %>% filter(job.id == 1)
-
-get_best <- function(data, criterion = c("AIC", "BIC", "Oracle"), measure = c("F1")) { 
-  
+# go over each experiment and select the lambda1, lambda2 value that 
+# has either the best AIC, BIC, F1 score... whatever you want to do. 
+# Do not forget to set 'maximum'!
+get_best_score_per_experiment <- function(data, var = c("aic", "bic", "F1"), maximum = FALSE) { 
+  if (maximum) { 
+    return(
+      data %>% group_by(job.id, repl) %>% slice(which.max(get(var[1]))) %>% ungroup()
+    )
+  } else {
+    return(
+      data %>% group_by(job.id, repl) %>% slice(which.min(get(var[1]))) %>% ungroup()
+    )
+  }
 }
+
+
+aic <- get_best_score_per_experiment(data, "aic", maximum = FALSE)
+bic <- get_best_score_per_experiment(data, "bic", maximum = FALSE)
+oracle <- get_best_score_per_experiment(data, "F1", maximum = TRUE)
